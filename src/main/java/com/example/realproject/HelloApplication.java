13d061545b5ff9af0d4e014784fd1357c1e6abd4
Abdulqader <144377302+5ymb@users.noreply.github.com>
2025-04-21 19:36:19 +0300

    InvestmentApp

--- src/main/java/com/example/realproject/HelloApplication.java
+++ src/main/java/com/example/realproject/HelloApplication.java
@@ -1,159 +1,199 @@
-package com.example.realproject;
-
-import javafx.application.Application;
-import javafx.geometry.HPos;
-import javafx.geometry.Pos;
-import javafx.scene.Scene;
-import javafx.scene.control.*;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
-import javafx.scene.text.Text;
-import javafx.stage.FileChooser;
-import javafx.stage.Stage;
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.text.DecimalFormat;
+import javafx.scene.paint.Color;
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
 
 public class HelloApplication extends Application {
-    private double balance=0.0;
-    private int year=0;
-    private static final DecimalFormat MONEY_FORMAT = new DecimalFormat("###,###.00");
 
-
+    private int year = 0;
+    private double balance = 0.0;
 
     @Override
-    public void start(Stage stage)  {
-        //Create objects//
-        Label intialBalanceLable = new Label("Initial Balance:");
-        Label intrestRateLable = new Label("Intrest Rate:");
+    public void start(Stage primaryStage) {
 
-        TextField intialBalanceTextField = new TextField();
-        TextField intrestRateTextField = new TextField();
 
-        Button calculateButton = new Button("Calculate");
-        Button exportButton = new Button("Export");
 
-        Text yearsText = new Text("0");
-        Text currentYearEarnText = new Text("0");
-        Text totalBalanceText = new Text("0");
+        //labels and textfields
+        Label initialBalanceLabel = new Label("Initial Balance");
+        TextField initialBalanceTF = new TextField();
 
-        Label yearsLabel = new Label("Years");
-        Label currentYearEarnLabel = new Label("Current Year Earnings");
-        Label totalBalanceLabel = new Label("Total Balance");
+        Label interestRateLabel = new Label("Interest Rate");
+        TextField interestRateTF = new TextField();
 
+        Label errorLetters = new Label();
+
+        HBox hbox1 = new HBox(25);
+        hbox1.getChildren().addAll(initialBalanceLabel, initialBalanceTF, interestRateLabel, interestRateTF, errorLetters);
+        hbox1.setAlignment(Pos.CENTER);
+
+
+
+
+
+        //textarea
         TextArea textArea = new TextArea();
-        textArea.setPrefWidth(2000);
-        textArea.setPrefHeight(2000);
+        textArea.setPrefSize(2000, 250);
         textArea.setEditable(false);
 
 
-        //Design//
-        yearsText.setFill(Color.RED);
-        currentYearEarnText.setFill(Color.RED);
-        totalBalanceText.setFill(Color.RED);
-        yearsLabel.setTextFill(Color.RED);
-        currentYearEarnLabel.setTextFill(Color.RED);
-        totalBalanceLabel.setTextFill(Color.RED);
-
-        yearsText.setFont(Font.font("", FontWeight.BOLD,40));
-        currentYearEarnText.setFont(Font.font("", FontWeight.BOLD,40));
-        totalBalanceText.setFont(Font.font("", FontWeight.BOLD,40));
-        yearsLabel.setFont(Font.font("Arial", FontWeight.BOLD,20));
-        currentYearEarnLabel.setFont(Font.font("Arial", FontWeight.BOLD,20));
-        totalBalanceLabel.setFont(Font.font("Arial", FontWeight.BOLD,20));
 
 
-        calculateButton.setPrefWidth(2000);
-        calculateButton.setPrefHeight(2000);
-        exportButton.setPrefWidth(2000);
-        exportButton.setPrefHeight(2000);
-
-        textArea.setStyle("-fx-border-color: lightgray;  -fx-border-width: 8;");
-        textArea.setPrefHeight(2000);
-        //Text panes//
-        HBox h1=new HBox(25);
-        h1.setAlignment(Pos.CENTER);
-        h1.getChildren().addAll(intialBalanceLable,intialBalanceTextField,intrestRateLable,intrestRateTextField);
-
-        GridPane pane=new GridPane();
-        pane.setAlignment(Pos.CENTER);
-        pane.setVgap(50);
-        pane.setHgap(70);
-        pane.setPrefSize(2000,2000);
-
-        pane.add(yearsText,0,0);
-        pane.add(currentYearEarnText,1,0);
-        pane.add(totalBalanceText,2,0);
-        pane.add(yearsLabel,0,1);
-        pane.add(currentYearEarnLabel,1,1);
-        pane.add(totalBalanceLabel,2,1);
-
-        GridPane.setHalignment(yearsText, HPos.CENTER);
-        GridPane.setHalignment(currentYearEarnText, HPos.CENTER);
-        GridPane.setHalignment(totalBalanceText, HPos.CENTER);
-        GridPane.setHalignment(yearsLabel, HPos.CENTER);
-        GridPane.setHalignment(currentYearEarnLabel, HPos.CENTER);
-        GridPane.setHalignment(totalBalanceLabel, HPos.CENTER);
-
-        VBox vbox = new VBox(25);
-        vbox.setAlignment(Pos.CENTER);
-        vbox.getChildren().addAll(h1,calculateButton,pane,textArea,exportButton);
 
 
-        //activate buttons//
-        calculateButton.setOnAction(e -> {
-            if(balance ==0.0) {
-                balance = Double.parseDouble(intialBalanceTextField.getText());
+        //buttons
+
+        
+        //button1
+        Button button1 = new Button("Calculate");
+
+        button1.setPrefHeight(50);
+        button1.setPrefWidth(2000);
+
+
+
+
+        //button2
+        Button exportBtn = new Button("Export");
+        exportBtn.setPrefHeight(50);
+        exportBtn.setPrefWidth(2000);
+
+
+
+
+
+
+        //labels that are in red
+        Label totalYears = new Label("0");
+        totalYears.setFont(Font.font("Arial", FontWeight.BOLD, 60));
+        totalYears.setTextFill(Color.RED);
+
+        Label years = new Label("Years");
+        years.setTextFill(Color.RED);
+        VBox yearsVBox = new VBox(40);
+        yearsVBox.getChildren().addAll(totalYears, years);
+        yearsVBox.setAlignment(Pos.CENTER);
+
+        Label currentYears = new Label("0");
+        currentYears.setFont(Font.font("Arial", FontWeight.BOLD, 60));
+        currentYears.setTextFill(Color.RED);
+        Label currentYearsEarning = new Label("Current years earning");
+        currentYearsEarning.setTextFill(Color.RED);
+        VBox currentYearsVBox = new VBox(40);
+        currentYearsVBox.getChildren().addAll(currentYears, currentYearsEarning);
+        currentYearsVBox.setAlignment(Pos.CENTER);
+
+        Label numBalance = new Label("0");
+        numBalance.setTextFill(Color.RED);
+        numBalance.setFont(Font.font("Arial", FontWeight.BOLD, 60));
+        Label totalBalance = new Label("Total Balance");
+        totalBalance.setTextFill(Color.RED);
+        VBox totalBalanceVBox = new VBox(40);
+        totalBalanceVBox.getChildren().addAll(numBalance, totalBalance);
+        totalBalanceVBox.setAlignment(Pos.CENTER);
+
+        HBox hbox2 = new HBox(30);
+        hbox2.setAlignment(Pos.CENTER);
+        hbox2.getChildren().addAll(yearsVBox, currentYearsVBox, totalBalanceVBox);
+
+
+
+
+
+
+        //vbox that contains labels, textfield and buttons
+        VBox vbox1 = new VBox(25);
+        vbox1.getChildren().addAll(hbox1, button1, hbox2, textArea, exportBtn);
+
+
+
+
+
+        //activate buttons
+        button1.setOnAction(e -> {
+
+            try{
+                if (balance == 0.0) {
+                    balance = Double.parseDouble(initialBalanceTF.getText());
+                }
+                double rate = Double.parseDouble(interestRateTF.getText()) / 100.0;
+
+                double currentYearEarn = balance * rate;
+                currentYearEarn = Math.round(currentYearEarn * 100.0) / 100.0;
+
+                balance = balance + currentYearEarn;
+                balance = Math.round(balance * 100.0) / 100.0;
+
+                year++;
+
+                totalYears.setText(Integer.toString(year));
+                currentYears.setText("SR " + currentYearEarn);
+                numBalance.setText("SR " + balance);
+
+                textArea.appendText(  "Year #" + year+ ": You Earned SR " + currentYearEarn + ", Total Balance SR " + balance + "\n"
+                );
+            }catch(Exception ex){
+
+                errorLetters.setText("You Only Can Put Numbers...");
+                errorLetters.setTextFill(Color.RED);
+
             }
-            double interestRate= Double.parseDouble(intrestRateTextField.getText());
 
-            double currentYearEarn = balance * (interestRate/100);
-            balance += currentYearEarn;
-            year++;
 
-            yearsText.setText(Integer.toString(year));
-            currentYearEarnText.setText("SR"+ MONEY_FORMAT.format(currentYearEarn));
-            totalBalanceText.setText("SR"+ MONEY_FORMAT.format(balance));
-
-            textArea.appendText("Year #:" + year + "You Earned: SR" + MONEY_FORMAT.format(currentYearEarn) + "and your total balance is SR" + MONEY_FORMAT.format(balance) + "\n");
         });
 
-        exportButton.setOnAction(e -> {
-            FileChooser fileChooser = new FileChooser();
-            fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("Text Files", "*.txt"));
-
-            File file = fileChooser.showSaveDialog(stage);
-
-            PrintWriter output = null;
-            if (file != null) {
-                try {
-                    output = new PrintWriter(file);
-                    output.print(textArea.getText());
-                } catch (IOException ex) {
-                    Alert alert = new Alert(Alert.AlertType.ERROR);
-                    alert.setTitle("Error");
-                    alert.setHeaderText("An error occurred");
-                    alert.setContentText(ex.getMessage());
-                    alert.showAndWait();
-                }
-                if (output != null) {
-                    output.close();
-                }
+        exportBtn.setOnAction(e -> {
+            FileChooser chooser1 = new FileChooser();
+            chooser1.getExtensionFilters().add(
+                    new FileChooser.ExtensionFilter("Text Files (*.txt)", "*.txt")
+            );
+            File file = chooser1.showSaveDialog(primaryStage);
+            if (file == null) {
+                return;
             }
+
+            try (PrintWriter writer1 = new PrintWriter(file);){
+
+                writer1.print(textArea.getText());
+
+
+            } catch (IOException ex) {
+                initialBalanceTF.appendText("Export Failed");
+
+
+
+
+            }
+
         });
 
-        Scene scene = new Scene(vbox, 800, 600);
-        stage.setTitle("Investment App");
-        stage.setScene(scene);
-        stage.show();
+        //lines that runs the program
+        StackPane root = new StackPane();
+        root.getChildren().addAll(vbox1);
+        Scene scene = new Scene(root, 1000, 625);
+        primaryStage.setTitle("Investment app");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
     }
+
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
+
 }
